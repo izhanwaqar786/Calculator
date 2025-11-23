b3f2e7465ff03d1536770d168e237af443c499a3
izhanwaqar786 <izhanwaqar185@gmail.com>
2025-11-23 12:06:49 +0500

    my calculator

---
+++ four.js
@@ -0,0 +1,6 @@
+var firstvalue = prompt("enter first value");
+var secondvalue = prompt("enter second value");
+
+var Result = Number(firstvalue) / Number(secondvalue);
+
+alert(Result);
---
+++ two.js
@@ -0,0 +1,7 @@
+var firstvalue = prompt("enter first value");
+var secondvalue = prompt("enter second value");
+
+var Result = Number(firstvalue) - Number(secondvalue);
+
+alert(Result);
+
